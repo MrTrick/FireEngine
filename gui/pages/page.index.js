f95509ee74c80d9deb89670d220ac5342ae03045
MrTrick <git_account@mindbleach.com>
2013-05-06 18:56:02 +1000

    Added client functionality to switch between different activity collection views

--- gui/pages/page.index.js
+++ gui/pages/page.index.js
@@ -37,27 +37,68 @@
 		}
 	});
 
-
+	var Activity_View_Switcher = Backbone.View.extend({
+		tagName: 'div',
+		className: 'btn-group',
+		views: {
+			active: 'Active',
+			all: 'All'
+		},
+		current: 'active',
+		render: function() {
+			this.$el.empty();
+			_.each(this.views, function(name, id) {
+				var $button = $("<button class='btn'>"+name+"</button>");
+				//Disable the current button
+				if (id == this.current) $button.attr({ id: ["show_"+id, "disabled"], disabled: true});
+				else $button.attr({ id:"show_"+id });
+				this.$el.append($button);
+			}, this);
+		},
+		events: {
+			'click #show_active': function() { this.switchTo('active'); },
+			'click #show_all': function() { this.switchTo('all'); }
+		},
+		switchTo: function(view) {
+			if (view == this.current) return;
+			this.current = view;
+			this.collection.view = view;
+			this.collection.fetch();
+			this.render();
+		}
+		
+	});
+	
 	App.Page.Index = Backbone.Marionette.Layout.extend({
 		template: _.template(
 			"<h2>Activities</h2>" +
 			"<div id='search'></div>" +
+			"<div id='view_switcher'></div>"+
 			"<div id='activities'></div>" +
 			"<p><a href=\"#create\">Create new</a></p>"
 		),
 		regions: {
 			search: "#search",
-			activities: "#activities"
+			activities: "#activities",
+			switcher: "#view_switcher"
 		},
 		initialize: function() {
-			//Fetch all activities (when fetched will be caught by the enclosing view)
 			var activities = new Activity.Collection();
+			
+			//Provide a view over a collection of activities 
+			var listview = new Activity_List({collection: activities});
+
+			//Provide a switcher to pick between which view is shown, linked to the same collection
+			var switcher = new Activity_View_Switcher({collection: activities});
+
+			this.on('render', function() { 
+				this.activities.show(listview);
+				this.switcher.show(switcher);
+			});
+			
+			//Fetch all activities (when fetched will be caught by the enclosing view)
 			activities.on('error', App.error.system);
 			activities.fetch();
-			
-			//Render them into a list within the page
-			var listview = new Activity_List({collection:activities});
-			this.on('render', function() { this.activities.show(listview); }); //Wait until the parent is rendered
 		}
 	});
 })(App);
